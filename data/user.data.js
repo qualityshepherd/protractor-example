cdff68a94751c0c754387fedc225a52e360b7b19
qualityshepherd <brian@qualityshepherd.fake>
2014-09-28 20:16:06 -0500

    Use maps to facilitate user data...

---
+++ data/user.data.js
@@ -0,0 +1,8 @@
+/**
+ * Store user data in maps for ease of use and readability...
+ */
+
+var UserData = function() {
+    this.testUser = {'username': 'test', 'password': 'test'};
+};
+module.exports = new UserData();
