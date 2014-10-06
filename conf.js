ae6bfdea522dd6753e5674ce93a023adcfcee25f
qualityshepherd <brian@qualityshepherd.fake>
2014-10-06 12:32:54 -0500

    adding files for travis...

--- conf.js
+++ conf.js
@@ -3,6 +3,7 @@ exports.config = {
 
 	//seleniumAddress: 'http://localhost:4444/wd/hub',
     seleniumServerJar: "/usr/local/lib/node_modules/protractor/selenium/selenium-server-standalone-2.42.2.jar",
+    chromeDriver: "/usr/bin/chromedriver",
 	specs: ['spec/*spec.js'],
     baseUrl: 'http://qualityshepherd.com/angular',
 //    sauceUser: ,
--- sauceConf.js
+++ sauceConf.js
@@ -3,10 +3,10 @@ exports.config = {
 
     //seleniumAddress: 'http://localhost:4444/wd/hub',
     seleniumServerJar: "/usr/local/lib/node_modules/protractor/selenium/selenium-server-standalone-2.42.2.jar",
+    chromeDriver: "/usr/bin/chromedriver",
     specs: ['spec/*spec.js'],
     baseUrl: 'http://qualityshepherd.com/angular',
-//    sauceUser: ,
-//    sauceKey: ,
+
 
     onPrepare: function(){
         global.dvr = browser.driver; // alias...
