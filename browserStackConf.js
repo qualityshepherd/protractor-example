2d84d37b8edde11c4e98279945535a7418b674b0
qualityshepherd <brian@qualityshepherd.fake>
2014-10-07 11:24:24 -0500

    added config for browserstack...

---
+++ browserStackConf.js
@@ -0,0 +1,43 @@
+exports.config = {
+    seleniumAddress: 'http://hub.browserstack.com/wd/hub',
+    specs: ['spec/*spec.js'],
+    baseUrl: 'http://qualityshepherd.com/angular',
+
+    onPrepare: function(){
+
+    },
+
+    capabilities: {
+        'browserName' : 'IE',
+        'browser_version' : '11.0',
+        'os' : 'Windows',
+        'os_version' : '8.1',
+        'browserstack.user': 'brine1',
+        'browserstack.key': '6dsjswP4BpbfE5Zfinvr'
+    },
+
+    // browserstack currently has issues with running multiple tests...
+//    multiCapabilities: [{
+//        'browserName' : 'IE',
+//        'browser_version' : '11.0',
+//        'os' : 'Windows',
+//        'os_version' : '8.1',
+//        'browserstack.user': 'brine1',
+//        'browserstack.key': '6dsjswP4BpbfE5Zfinvr'
+//    }, {
+//        'browser': 'Safari',
+//        'browser_version': '7.0',
+//        'os': 'OS X',
+//        'os_version': 'Mavericks',
+//        'browserstack.user': 'brine1',
+//        'browserstack.key': '6dsjswP4BpbfE5Zfinvr'
+//    }],
+
+    jasmineNodeOpts: {
+        isVerbose: true,
+        includeStackTrace: true,
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    }
+};
+
