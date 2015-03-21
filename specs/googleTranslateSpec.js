17d8c7f4087723efb0b5d99edfd9fab6df2a6dff
brine <brian@qualityshepherd.com>
2015-03-21 13:28:45 -0500

    Fixing a test that only fails in CI...

--- specs/googleTranslateSpec.js
+++ specs/googleTranslateSpec.js
@@ -1,10 +1,7 @@
 var translatePage = require('../pages/googleTranslatePage');
 
 describe ('google translate test with pages objects', function() {
-    // I'm done with this test. It's flaky and succeeds locally but fails on CI.
-    // but since this is a demo, I just stopped caring why. For extra credit,
-    // feel free to fix it and send me a pull request.
-    xit('should translate Spanish to English', function() {
+    it('should translate Spanish to English', function() {
         // given at the translate pages...
         translatePage.to();
 
