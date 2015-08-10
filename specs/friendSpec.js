f88bc358834edf1178e318b22d0792e6fe18a195
brine <brian@qualityshepherd.com>
2015-08-09 19:57:00 -0500

    remove deprecated tests/page objects...

--- specs/friendSpec.js
+++ specs/friendSpec.js
@@ -4,7 +4,7 @@
 var friendPage = require('../pages/friendPage');
 var chance = require('../node_modules/chance').Chance();
 
-describe ('friend app', function() {
+describe ('angular app', function() {
 
 	beforeEach(function() {
         friendPage.to();
