61600f92c478c1ea700d18d0ab7eb8ed1b8f98d6
brine <brian@qualityshepherd.com>
2017-04-20 10:05:39 -0500

    updates for protractor 5.x

--- conf.js
+++ conf.js
@@ -5,7 +5,7 @@ exports.config = {
      *  will auto-start selenium if you uncomment the jar, or connect directly to chrome/firefox
      *  if you uncomment directConnect. 
      */
-    //seleniumServerJar: "node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.3.jar",
+    //seleniumServerJar: "node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.3.1.jar",
     directConnect: true,
     
     specs: ['specs/*Spec.js'],
@@ -13,8 +13,6 @@ exports.config = {
     framework: 'jasmine2',
 
     onPrepare: function(){
-        // set implicit wait times in ms...
-        browser.manage().timeouts().implicitlyWait(5000);
         // set browser size...
         browser.manage().window().setSize(1024, 800);
 
@@ -24,8 +22,25 @@ exports.config = {
     },
 
     capabilities: {
-		browserName: 'chrome'
-	},
+        browserName: 'chrome',
+        shardTestFiles: true,
+        maxInstances: 2,
+        chromeOptions: {
+            args: [
+                // disable chrome's wakiness
+                '--disable-infobars',
+                '--disable-extensions',
+                'verbose', 
+                'log-path=/tmp/chromedriver.log'
+            ],
+            prefs: {
+                // disable chrome's annoying password manager
+                'profile.password_manager_enabled': false,
+                'credentials_enable_service': false,
+                'password_manager_enabled': false
+            }
+        }
+    },
 
     jasmineNodeOpts: {
         showColors: true,
